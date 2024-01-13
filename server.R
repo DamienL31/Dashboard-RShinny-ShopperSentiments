c64d3a9233dd97d8e3330670008a06723c3bdf6f
corentin gilles <corentingilles81@gmail.com>
2024-01-13 13:20:11 +0100

    avancement de l'architecture

--- server.R
+++ server.R
@@ -1,8 +1,36 @@
-# server.R
-
 library(shiny)
 
 shinyServer(function(input, output, session) {
   
+  # Variable pour suivre l'état du bouton
+  graphState <- reactiveVal(TRUE)
+  
+  # Observer le bouton
+  observeEvent(input$toggleButton, {
+    graphState(!graphState()) # Basculer l'état
+  })
+  
+  # Afficher ce qu'on veut  en fonction de l'état
+  output$dynamicGraph <- renderUI({
+    if(graphState()) {
+      plotOutput("graph1", height = "500px")
+    } else {
+      plotOutput("graph2", height = "500px")
+    }
+  })
+  
+  # Génération des graphiques (Remplacez avec votre propre logique de graphique)
+  output$graph1 <- renderPlot({ 
+    # Votre code pour générer le premier graphique
+    # on fait une map mais ça devrait marcher quand même 
+    
+  })
+  output$graph2 <- renderPlot({ 
+    # Votre code pour générer le second graphique
+    # ici on va pas générer un graphique mais j'imagine que c'est la même chose
+    
+  })
+  
+  #suite du code où y aura vos calculs
   
 })
--- ui.R
+++ ui.R
@@ -1,27 +1,46 @@
-# ui.R
-
 library(shiny)
 library(shinydashboard)
 
-shinyUI(
-  dashboardPage(
-    dashboardHeader(title = "Mon Dashboard"),
-    dashboardSidebar(
-    #Partie Filtre
-    ),
-    dashboardBody(
-
-      tabsetPanel(
-        tabPanel("Menu",
-                h2("Bienvenue sur le tableau de bord!"),
-                p("C'est votre page d'accueil. Vous pouvez personnaliser cela.")
-                #Partie Menu
-        ),
-        tabPanel("Graphique",
-                plotOutput("plot1", height = "400px")
-                #Partie Graphique
-        )
-      )
-    )
-  )
-)
+ui <- dashboardPage(
+  dashboardHeader(title = "Mon Dashboard"),
+  dashboardSidebar(
+    # Partie Filtre
+  ),
+  dashboardBody(
+    tabsetPanel(
+      tabPanel("Menu",
+               fluidRow(
+                 column(1), # Colonne vide pour le padding
+                 column(3,
+                        box(title = "Note moyenne ",
+                            style = "height: 70px;",
+                            textOutput("Moyenne_note")
+                        )
+                 ),
+                 column(1), # Colonne vide pour l'espace
+                 column(3,
+                        box(title = "Nombre total d'avis",
+                            style = "height: 70px;",
+                            textOutput("nb_avis")
+                        )
+                 ),
+                 column(1), # Colonne vide pour l'espace
+                 column(3,
+                        box(title = HTML("Ratio de notes 5 <i class='fa fa-star'></i>"),
+                            style = "height: 70px;",
+                            textOutput("ratio_5")
+                        )
+                 )
+               ),
+               fluidRow(
+                 actionButton("toggleButton", "On/Off"),
+                 uiOutput("dynamicGraph")
+               )
+      ), # Fin du premier onglet "Menu"
+      tabPanel("Graphique",
+               plotOutput("plot1", height = "400px")
+               # Partie Graphique
+      ) # Fin du deuxième onglet "Graphique"
+    ) # Fin de tabsetPanel
+  ) # Fin de dashboardBody
+) # Fin de dashboardPage
