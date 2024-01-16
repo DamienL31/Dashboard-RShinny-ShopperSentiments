ad7bfc98d078908b8ac5729fb341dc630d259c2c
Damien Lauger <152622955+DamienL31@users.noreply.github.com>
2024-01-16 19:31:30 +0100

    Ajout global + library

--- global.R
+++ global.R
@@ -1,1 +1,13 @@
 data <- read.csv("DATA/TeePublic_review.csv")
+
+#voir les différents types de données 
+
+glimpse(data)
+
+
+#colonne date et month en INT transformation et concaténation 
+data$date <- as.character(data$date)
+data$month <- as.character(data$month)
+
+data$mois_annee <- paste(data$month, data$date, sep = "-")
+data$mois_annee <- as.Date(paste("01", data$mois_annee, sep = "-"), format = "%d-%m-%Y")
